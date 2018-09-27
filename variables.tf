112a9ea2a724fdc6d5640c8c6ffdd7ce63675bd7
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-27 14:20:37 -0700

    terraform fmt after description add

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 # VNet CIDR
 variable "vnet_cidr" {
   description = "vnet cidr"
-  default = "10.32.0.0/16"
+  default     = "10.32.0.0/16"
 }
 
 # Public address prefix
 variable "public_cidr" {
   description = "public cidr"
-  default = "10.32.0.0/22"
+  default     = "10.32.0.0/22"
 }
 
 # Private address prefix
 variable "private_cidr" {
   description = "private cidr"
-  default = "10.32.4.0/22"
+  default     = "10.32.4.0/22"
 }
 
 # Location (region)
@@ -34,12 +34,12 @@ variable "resource_group_name" {
 # Add special tags to the resources created by this module
 variable "tags" {
   description = "Add custom tags to all resources"
-  type    = "map"
-  default = {}
+  type        = "map"
+  default     = {}
 }
 
 # Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
   description = "Format the hostname inputs are index+1, region, cluster_name"
-  default = "network-%[1]d-%[2]s"
+  default     = "network-%[1]d-%[2]s"
 }
